268970683570f9561ba086ba6e9b84cbe504cb3b
alonso <alonso.salas@usach.cl>
2024-05-09 01:38:28 -0400

    añadido router

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
 import { BootstrapVue3} from 'bootstrap-vue-3'
-import router from './routes/index'
+import router from './routes/index.js'
 
 const app = createApp(App)
 
